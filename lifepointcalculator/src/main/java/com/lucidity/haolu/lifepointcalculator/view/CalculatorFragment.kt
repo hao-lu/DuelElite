0865b2b030d59df9c1b652313bdcca2b2b27043f
Hao Lu <haolu2013@gmail.com>
2020-01-23 23:30:21 -0800

    Add cumulated CalculatorFragment

--- lifepointcalculator/src/main/java/com/lucidity/haolu/lifepointcalculator/view/CalculatorFragment.kt
+++ lifepointcalculator/src/main/java/com/lucidity/haolu/lifepointcalculator/view/CalculatorFragment.kt
@@ -1,7 +1,11 @@
 package com.lucidity.haolu.lifepointcalculator.view
 
-import androidx.constraintlayout.widget.ConstraintLayout
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.lucidity.haolu.lifepointcalculator.R
 
 class CalculatorFragment : Fragment() {
 
@@ -9,4 +13,12 @@ class CalculatorFragment : Fragment() {
         fun newInstance() = CalculatorFragment()
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_calculator, container, false)
+//        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 }
