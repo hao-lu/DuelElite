284ac23509caeda2c537e416f4f4974ebdadd8f9
Hao Lu <haolu2013@gmail.com>
2020-01-25 12:43:12 -0800

    Add transparent navigation bar

--- app/src/main/java/com/lucidity/haolu/duelking/view/activity/BottomNavigationActivity.kt
+++ app/src/main/java/com/lucidity/haolu/duelking/view/activity/BottomNavigationActivity.kt
@@ -1,8 +1,11 @@
 package com.lucidity.haolu.duelking.view.activity
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.lucidity.haolu.duelking.R
+import com.lucidity.haolu.duelking.databinding.ActivityBottomNavigationBinding
 import com.lucidity.haolu.lifepointcalculator.view.CalculatorFragment
 
 class BottomNavigationActivity : AppCompatActivity() {
@@ -10,11 +13,18 @@ class BottomNavigationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_bottom_navigation)
+        DataBindingUtil.setContentView<ActivityBottomNavigationBinding>(
+            this,
+            R.layout.activity_bottom_navigation
+        )
 
         supportFragmentManager.beginTransaction()
-                .replace(R.id.container, CalculatorFragment())
-                .commit()
+            .replace(R.id.container, CalculatorFragment())
+            .commit()
 
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
     }
 }
