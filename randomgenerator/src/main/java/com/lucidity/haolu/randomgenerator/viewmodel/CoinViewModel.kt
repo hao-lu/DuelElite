90e2500afa18f3a30c28d6dc3e3ccefeb46783bb
Hao Lu <haolu2013@gmail.com>
2020-08-17 23:02:42 -0700

    RandomGenerator use base

--- randomgenerator/src/main/java/com/lucidity/haolu/randomgenerator/viewmodel/CoinViewModel.kt
+++ randomgenerator/src/main/java/com/lucidity/haolu/randomgenerator/viewmodel/CoinViewModel.kt
@@ -3,7 +3,7 @@ package com.lucidity.haolu.randomgenerator.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.lucidity.haolu.randomgenerator.Event
+import com.lucidity.haolu.base.Event
 import com.lucidity.haolu.randomgenerator.model.RandomGenerator
 
 class CoinViewModel : ViewModel() {
