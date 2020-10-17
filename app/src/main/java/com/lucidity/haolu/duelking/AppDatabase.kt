ca059a0c549a8607f4d910c8cf90a4e314d4c276
Hao Lu <haolu2013@gmail.com>
2020-10-17 13:30:19 -0700

    Fix release build

--- app/src/main/java/com/lucidity/haolu/duelking/AppDatabase.kt
+++ app/src/main/java/com/lucidity/haolu/duelking/AppDatabase.kt
@@ -7,30 +7,30 @@ import androidx.room.RoomDatabase
 import com.lucidity.haolu.searchcards.room.entity.Card
 import com.lucidity.haolu.searchcards.room.dao.CardDao
 
-@Database(entities = arrayOf(Card::class), version = 1)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun cardDao(): CardDao
-
-    companion object {
-        var instance: AppDatabase? = null
-        fun getInstance(context: Context): AppDatabase? {
-            if (instance == null) {
-                synchronized(AppDatabase::class.java) {
-                    if (instance == null) {
-                        instance =
-                            buildDataBaseBuild(context)
-                    }
-                }
-            }
-            return instance
-        }
-
-        private fun buildDataBaseBuild(context: Context): AppDatabase {
-            return Room.databaseBuilder(
-                context,
-                AppDatabase::class.java, "appdb"
-            )
-                .build()
-        }
-    }
-}
+//@Database(entities = arrayOf(Card::class), version = 1)
+//abstract class AppDatabase : RoomDatabase() {
+//    abstract fun cardDao(): CardDao
+//
+//    companion object {
+//        var instance: AppDatabase? = null
+//        fun getInstance(context: Context): AppDatabase? {
+//            if (instance == null) {
+//                synchronized(AppDatabase::class.java) {
+//                    if (instance == null) {
+//                        instance =
+//                            buildDataBaseBuild(context)
+//                    }
+//                }
+//            }
+//            return instance
+//        }
+//
+//        private fun buildDataBaseBuild(context: Context): AppDatabase {
+//            return Room.databaseBuilder(
+//                context,
+//                AppDatabase::class.java, "appdb"
+//            )
+//                .build()
+//        }
+//    }
+//}
